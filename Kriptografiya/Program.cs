735da903f27a5a4b34e24e03263828fac192810a
Kubik Nastya <aknk97@mail.ru>
2017-09-16 17:40:14 +0300

    Добавленны новые виды шифрования на форму.

---
+++ Kriptografiya/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Kriptografiya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write path of file for decoder: ");
+            string filename = Console.ReadLine();
+
+            Console.Write("Write key: ");
+            string word = Console.ReadLine();
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
